162bdcd2f0b3004e00cfe655760f5e35e2ee250a
Marina <m.fedotova1911@mail.ru>
2020-11-23 02:35:47 +0300

    BaseEnd

---
+++ BaseLab/BaseLab/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseLab
+{
+    class Operations
+    {
+        static public double Calc(double op1, string sign, double op2)
+        {
+            switch (sign)
+            {
+                case "+":
+                    return op1 + op2;
+                case "-":
+                    return op1 - op2;
+                case "/":
+                    return op1 / op2;
+                case "*":
+                    return op1 * op2;
+                default:
+                    Console.WriteLine("Операция выбрана неверно или не существует!");
+                    return 0;
+            }
+        }
+    }
+}
