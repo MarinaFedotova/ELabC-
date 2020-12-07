4bb11199892e0a551f16614303f23601f641f43f
Marina <m.fedotova1911@mail.ru>
2020-12-08 00:34:45 +0300

    NUnitEnd

---
+++ NUnitTestLab/NUnitTestLab/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestLab
+{
+    static class Calc
+    {
+        static public double Plus(double operand1, double operand2)
+        {
+            return operand1 + operand2;
+        }
+        
+        static public double Minus(double operand1, double operand2) 
+        {
+            return operand1 - operand2;
+        }
+        
+        static public double Division(double operand1, double operand2)
+        {
+            if (operand2 == 0)
+                return 0;
+            return operand1 / operand2;
+        }
+        
+        static public double Multiplication(double operand1, double operand2)
+        {
+            return operand1 * operand2;
+        }
+    }
+}
---
+++ NUnitTestLab/NUnitTestLab/TestValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestLab
+{
+    class TestValue
+    {
+        static object[] DivisionCases =
+        {
+            new object[] { 10, 0, 0 },
+            new object[] { 10, -2, -5 },
+            new object[] { 10, 0.2, 50 },
+            new object[] { 10, 4, 2.5 }
+        };
+        
+        static object[] MultiplicationCases =
+        {
+            new object[] { 0, 0, 0 },
+            new object[] { -1, -1, 1 },
+            new object[] { 10, 0, 0 },
+            new object[] { 10, 0.2, 2 }
+        };
+        static object[] OtherCases =
+        {
+            new object[] { 0, 0, 0 },
+            new object[] { 10, 0, 10 },
+        };
+    }
+}
